332521ba673eb83d6bf3ceb2bfeb0362c82db250
vivi0511-ship-it <shaivi@example.com>
2026-09-15 23:51:50 +0530

    Remove marquee banner from home page hero layout to match Home page-1.png reference

--- src/components/HomeHero.jsx
+++ src/components/HomeHero.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useAudio } from '../context/AudioContext'
 import TurntablePlayer from './TurntablePlayer'
-import MarqueeBanner from './MarqueeBanner'
 import './HomeHero.css'
 
 function HomeHero() {
@@ -201,20 +200,6 @@ function HomeHero() {
           </div>
         </div>
       </div>
-
-      {/* Landing Page Horizontal Marquee Banner between sections */}
-      <MarqueeBanner
-        items={[
-          'UI/UX DESIGNER',
-          'PRODUCT STRATEGY',
-          'USER RESEARCH',
-          'INFORMATION ARCHITECTURE',
-          'INTERACTIVE PROTOTYPING',
-          'DESIGN SYSTEMS',
-          'ACCESSIBLE INTERFACES'
-        ]}
-        className="landing-variant"
-      />
     </main>
   )
 }
